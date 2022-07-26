5c28a39d6e232fc8fadab7f8d5a04d2100a088f5
ajanardhanan <akjanardhan@gmail.com>
2022-07-26 18:10:48 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Anil Website',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [
